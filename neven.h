626bc5f85e2405f2d0a42b2e6a8309c09ded4470
Liu Qishuai <lqs@lqs.me>
2012-02-08 23:46:31 +0800

    fix a typo

--- neven.h
+++ neven.h
@@ -16,7 +16,7 @@ struct neven_face {
         float eyedist;
 };
 
-struct neven_env *neven_init(int w, int h, int maxFaces);
+struct neven_env *neven_create(int w, int h, int maxFaces);
 void neven_destroy(struct neven_env *env);
 int neven_detect(struct neven_env *env, void *bwbuffer);
 void neven_get_face(struct neven_env *env, struct neven_face *face, int index);
